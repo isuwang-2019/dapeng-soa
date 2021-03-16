9999255fba904c38c21afa4ed2a0cab93b9156a1
蒙初开 <mengchukai@iplas.com.cn>
2021-03-16 10:47:13 +0800

    kuaisu-2.1.1.3

--- dapeng-code-generator/src/main/scala/com/github/dapeng/code/generator/JavaGenerator.scala
+++ dapeng-code-generator/src/main/scala/com/github/dapeng/code/generator/JavaGenerator.scala
@@ -512,21 +512,20 @@ class JavaGenerator extends CodeGenerator {
       <div>package {struct.namespace};
 
         import java.util.Optional;
-        import io.swagger.annotations.ApiModel;
-        import io.swagger.annotations.ApiModelProperty;
+        import io.swagger.v3.oas.annotations.media.Schema
 
         /**
         {notice}
         *{struct.doc}
         **/
-        @ApiModel
+        @Schema
         public class {struct.name}<block>
         {toFieldArrayBuffer(struct.getFields).map{(field : Field) =>{
           <div>
             /**
             *{field.doc}
             **/
-            @ApiModelProperty(value = "{field.doc.replaceAll("\\s+", "").replaceAll("\\n","").replaceAll("\"","").replaceAll("\\\\","\\\\\\\\")}" {if(!field.isOptional) <div> ,required = true </div>} )
+            @Schema(description = "{field.doc.replaceAll("\\s+", "").replaceAll("\\n","").replaceAll("\"","").replaceAll("\\\\","\\\\\\\\")}" {if(!field.isOptional) <div> ,required = true </div>} )
             {if(field.isPrivacy)  <div>private</div> else <div>public</div>} {if(field.isOptional) <div>Optional{lt}</div>}{toDataTypeTemplate(field.isOptional, field.getDataType)}{if(field.isOptional) <div>{gt}</div>} {field.name} {if(field.isOptional) <div>= Optional.empty()</div> else {
             field.dataType.kind match {
               case KIND.LIST => <div>= new java.util.ArrayList()</div>
