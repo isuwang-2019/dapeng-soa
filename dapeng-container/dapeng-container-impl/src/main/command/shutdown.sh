b25b52babeb8487a1675d382231b4976eff97edd
chinazmc <857578417@qq.com>
2021-05-24 16:01:53 +0800

    shutdown.sh

--- dapeng-container/dapeng-container-impl/src/main/command/shutdown.sh
+++ dapeng-container/dapeng-container-impl/src/main/command/shutdown.sh
@@ -15,25 +15,71 @@ else
 fi
 
 cd $workdir
+echo "begin to shutdown..."
+#根据端口号查一下pid
+PID=$(ps -ef   |grep java |grep -v "grep" |awk '{print $2}')
+if [ ! -n "$PID" ];
+  then
+    echo ""
+    echo "pid is not exist"
+    exit 1
+fi
+kill $PID
 
-cat $workdir/../logs/pid.txt | while read line
-do
-  kill $line
-  echo ...is killing pid $line...
-  while true;
-  do
-    pro=$(ps $line | grep $line | wc -l)
-    echo '...please wait...'
-    if [ $pro -ge 1 ]
+#看看进程还在吗
+UP_STATUS=$(ps -ef   |grep java |grep -v "grep" |awk '{print $2}' | wc -l)
+
+if (($UP_STATUS <= 0));
+  then
+    echo "shutdown succesful..."
+  else
+    echo ""
+    echo "pid = $PID"
+    TIME_OUT=90
+    TIME_COUNT=0
+    while [ $TIME_COUNT -le $TIME_OUT ]
+    do
+      UP_STATUS=$(ps -ef   |grep java |grep -v "grep" |awk '{print $2}' | wc -l)
+      if (($UP_STATUS <= 0))
       then
-        sleep 1
-      else break
+        TIME_COUNT=999
+      else
+        TIME_COUNT=$(($TIME_COUNT+1))
+      fi
+      echo "waiting for shutdown ..."
+      sleep 1
+    done
+    if (($UP_STATUS <= 0))
+      then
+        echo ""
+        echo "shutdown successful..."
+      else
+        echo ""
+        ps -ef | grep "$PID" | grep -v grep | grep "$SERVER_NAME"
+        echo ""
+        echo "shutdown failed..."
     fi
-  done
-done
 
-echo '...shutdown finish....'
+fi
 
-rm -rf $workdir/../logs/pid.txt
-
-touch $workdir/../logs/pid.txt
+#cat $workdir/../logs/pid.txt | while read line
+#do
+#  kill $line
+#  echo ...is killing pid $line...
+#  while true;
+#  do
+#    pro=$(ps $line | grep $line | wc -l)
+#    echo '...please wait...'
+#    if [ $pro -ge 1 ]
+#      then
+#        sleep 1
+#      else break
+#    fi
+#  done
+#done
+#
+#echo '...shutdown finish....'
+#
+#rm -rf $workdir/../logs/pid.txt
+#
+#touch $workdir/../logs/pid.txt
